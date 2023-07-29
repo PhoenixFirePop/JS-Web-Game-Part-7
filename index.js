ba9efd041ffce25d835425f4312f9083b35d84fb
Diana T <135925668+PhoenixFirePop@users.noreply.github.com>
2023-07-29 10:28:36 -0500

    npc movement async adustment

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
 async function moveNPC(){
- await npc.walkNorth(1400)
- await npc.walkEast(1400)
- await npc.walkSouth(300)
- await npc.walkEast(1500)
- await npc.walkSouth(1500)
- await npc.walkWest(2700)
- await npc.walkNorth(1400)
+    await npc.walkNorth(1400)
+    await npc.walkEast(1400)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(1400)
 }
+
 moveNPC()
 
 // Create the inventory
